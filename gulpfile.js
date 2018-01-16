366e71eda1cb973e998e277ca319c9fb70606fcf
luan0ap <luanap.kun@gmail.com>
2018-01-16 14:29:39 -0200

    Removidos comentarios do git

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const gulp = require('gulp');
 const sourcemaps = require('gulp-sourcemaps');
 const autoprefixer = require('gulp-autoprefixer');
@@ -36,43 +35,4 @@ gulp.task('watch', function(){
     gulp.watch('public/src/sass/*.scss', ["watch:sass"]);
 });
 
-=======
-const gulp = require('gulp');
-const sourcemaps = require('gulp-sourcemaps');
-const autoprefixer = require('gulp-autoprefixer');
-const concat = require('gulp-concat');
-const minifyCSS = require('gulp-csso');
-const imagemin = require('gulp-imagemin');
-const sass = require('gulp-sass');
-
-gulp.task('watch:image', () => {
-    return gulp.src([
-        'public/src/images/*.png', 
-        'public/src/images/*.jpg', 
-        'public/src/images/*.jpeg'
-        ])
-        .pipe(imagemin())
-        .pipe(gulp.dest('public/src/images/min'));
-});
-
-gulp.task('watch:sass', function () {
-    return gulp.src('public/src/sass/*.scss')
-            .pipe(sourcemaps.init())
-            .pipe(sass()).on('error', sass.logError)
-            .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
-            .pipe(sourcemaps.write())
-            .pipe(minifyCSS())
-            .pipe(gulp.dest('public/src/css/min'))
-});
-
-gulp.task('watch', function(){
-    gulp.watch([
-        'public/src/images/*.png', 
-        'public/src/images/*.jpg', 
-        'public/src/images/*.jpeg'
-    ], ["watch:image"]);
-    gulp.watch('public/src/sass/*.scss', ["watch:sass"]);
-});
-
->>>>>>> 837f5ca07b1d8f9fbefac2cd9301058fe174b4cd
 gulp.task('default',  ['watch']);
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const webpack = require('webpack');
 const ENV = "dev";
 const { join } = require('path')
@@ -31,37 +30,3 @@ module.exports = {
         }]
     }
 };
-=======
-const webpack = require('webpack');
-const ENV = "production";
-const { join } = require('path')
-
-module.exports = {
-    context: __dirname,
-    watch: true,
-    devtool: false,
-    entry: {
-        app: './public/src/js/app.js',
-        login: './public/src/js/login.js',
-        bots: './public/src/js/bots.js'
-    },
-    output: {
-        path: join(__dirname, 'public', 'src', 'js', 'min'),
-        filename: '[name].js'
-    },
-    plugins: ((ENV === "production") ? [
-        new webpack.optimize.UglifyJsPlugin({
-            sourceMap: true,
-            uglifyOptions: {
-                ecma: 8
-            }
-        })
-    ] : []),
-    module: {
-        rules: [{
-            test: /\.(js)$/,
-            loader: 'babel-loader'
-        }]
-    }
-}
->>>>>>> 837f5ca07b1d8f9fbefac2cd9301058fe174b4cd
