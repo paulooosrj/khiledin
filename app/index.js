6493d59101ae881d12a77f0071b040e3fdc7c9d2
Paulo Sergio <jskhanframework@gmail.com>
2020-03-06 17:17:13 -0300

    Add files via upload

--- app/index.js
+++ app/index.js
@@ -1,51 +1,89 @@
-const express = require('express'),
-      compression = require('compression'),
-      app = express(),
-      server = require('http').Server(app),
-      path = require('path'),
-      cookieParser = require('cookie-parser'),
-      bodyParser = require('body-parser'),
-      minify = require('express-minify'),
-      session = require('express-session'),
-      MongoStore = require('connect-mongo')(session),
-      db = require('./database/index'),
-      used = (...u) => u.map(uu => Array.isArray(uu) ? app.use(...uu) : app.use(uu)),
-      seted = (...s) => s.map(ss => app.set(...ss)),
-      sessionn = session({
-        secret: 'keyboard cat',
-        resave: false,
-        store: new MongoStore({ mongooseConnection: db.connection }),
-        saveUninitialized: true,
-        cookie: { expires: new Date(253402300000000) }
-      });
+const express = require('express');
+const compression = require('compression');
+const server = require('http');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const minify = require('express-minify');
+const session = require('express-session');
+let MongoStore = require('connect-mongo');
+const db = require('./database/index');
 
 global.log = require('signale');
 global.fs = require('fs');
 global.fileUpload = require('express-fileupload');
-global.io = require('socket.io')(server);
+global.io = require('socket.io');
 global.bots = require('./public/src/js/modules/bots/bots');
 
-seted(
-  [ 'port', process.env.PORT || 3000 ],
-  [ 'views', path.join(__dirname, 'views') ],
-  [ 'view engine', 'ejs' ],
-  [ 'trust proxy', 1 ]
-);
+class App {
 
-used(
-  cookieParser(),
-  sessionn,
-  fileUpload(),
-  compression(),
-  minify(),
-  bodyParser.urlencoded({ extended: false }),
-  bodyParser.json(),
-  [ '/', express.static(__dirname + '/public') ]
-);
+  constructor(){
 
-const routes = require('./routes/index')(app);
-const sockets = require('./sockets')(io);
+    this.app = express();
+    this.config();
+    this.listen();
 
-server.listen(app.get('port'), () =>
-  log.success(`[Listening on]: ${ app.get('port') }`)
-);
+  }
+
+  config(){
+
+    this.httpServer = this.httpServer();
+
+    // insere sessao no mongodb
+    MongoStore = MongoStore(session);
+
+    const sessionInit = session({
+      secret: 'keyboard cat',
+      resave: false,
+      store: new MongoStore({ mongooseConnection: db.connection }),
+      saveUninitialized: true,
+      cookie: { expires: new Date(253402300000000) }
+    });
+
+    // setar metodos essenciais
+    this.set(
+      [ 'port', process.env.PORT || 3000 ],
+      [ 'views', path.join(__dirname, 'views') ],
+      [ 'view engine', 'ejs' ],
+      [ 'trust proxy', 1 ]
+    );
+
+    this.use(
+      cookieParser(),
+      sessionInit,
+      fileUpload(),
+      compression(),
+      minify(),
+      bodyParser.urlencoded({ extended: false }),
+      bodyParser.json(),
+      [ '/', express.static(__dirname + '/public') ]
+    );
+
+    io = io(this.httpServer);
+
+    const routes = require('./routes/index')(this.app);
+    const sockets = require('./sockets')(io);
+
+  }
+
+  httpServer(){
+    return server.Server(this.app);
+  }
+
+  use(...uses){
+    return uses.map((useConfig) => Array.isArray(useConfig) ? this.app.use(...useConfig) : this.app.use(useConfig));
+  }
+
+  set(...seteds){
+    return seteds.map((setConfig) => this.app.set(...setConfig));
+  }
+
+  listen(){
+    this.httpServer.listen(this.app.get('port'), () =>
+      log.success(`[Listening on]: ${ this.app.get('port') }`)
+    );
+  }
+
+}
+
+module.exports = new App();
---
+++ app/services/bots.service.js
@@ -0,0 +1,50 @@
+const path = require('path');
+
+class BotsService {
+
+    bots(req, res) {
+      res.render('bots');
+    }
+
+    botFind(req, res) {
+
+      const { params: { id: paramsId = '' } } = req;
+
+      let bot = bots.find(({ id }) => id === paramsId);
+
+      if(typeof bot === "object"){
+
+        log.success('[Find Bot]: ' + JSON.stringify(bot, null, 4));
+        const botFolder = path.resolve(__dirname, '../public/');
+
+        fs.readFile(botFolder + '/' + bot.code, function read(err, data) {
+            if (err) res.redirect('../bots');
+            if(data === "undefined") res.redirect('../bots');
+            res.render('includes/viewbot', {
+              'id': bot.id,
+              'code': data
+            });
+        });
+
+      }else{
+        res.send('Não existe!!');
+      }
+
+    }
+
+    code(req, res) {
+
+      const { params: { file = '' } } = req;
+
+      const codeFolder = path.resolve(__dirname, '../public/src/uploads/arquivo/' + file);
+      const code = fs.readFileSync(codeFolder, {
+        encoding: 'utf8'
+      });
+
+      res.render('code', { code });
+
+    }
+
+}
+
+module.exports = new BotsService();
